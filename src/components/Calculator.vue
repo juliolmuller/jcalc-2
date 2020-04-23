186caf11b24f25eb92166b404ddd932072da95e2
Julio Muller <juliodolago@gmail.com>
2020-04-23 20:01:45 -0300

    Implemented operations logic

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -11,9 +11,9 @@
     <Button label="5" @click="addDigit" />
     <Button label="6" @click="addDigit" />
     <Button label="-" @click="setOperation" operator />
-    <Button label="3" @click="addDigit" />
-    <Button label="2" @click="addDigit" />
     <Button label="1" @click="addDigit" />
+    <Button label="2" @click="addDigit" />
+    <Button label="3" @click="addDigit" />
     <Button label="+" @click="setOperation" operator />
     <Button label="0" @click="addDigit" double />
     <Button label="." @click="addDigit" />
@@ -45,7 +45,24 @@ export default {
       Object.assign(this.$data, this.$options.data())
     },
     setOperation(operator) {
-      console.log('Operação', operator)
+      const isEquals = (operator === '=')
+      if (!isEquals && this.current === 0) {
+        this.operation = operator
+        this.current = 1
+        this.clearDisplay = true
+      } else {
+        try {
+          this.operands[0] = eval(
+            `${this.operands[0]} ${this.operation} ${this.operands[1]}`,
+          )
+        } catch (e) {
+          this.$emit('error', e)
+        }
+        this.displayValue = this.operands[0]
+        this.operation = isEquals ? this.operation : operator
+        this.current = isEquals ? 0 : 1
+        this.clearDisplay = isEquals
+      }
     },
     addDigit(digit) {
       if (digit === '.' && this.displayValue.includes('.')) {
@@ -54,10 +71,8 @@ export default {
       const clearDisplay = (this.displayValue === '0') || this.clearDisplay
       const currentValue = clearDisplay ? '' : this.displayValue
       this.displayValue = `${currentValue}${digit}`
+      this.operands[this.current] = this.displayValue
       this.clearDisplay = false
-      if (digit !=='.') {
-        this.operands[this.current] = Number(this.displayValue)
-      }
     },
   },
 }
