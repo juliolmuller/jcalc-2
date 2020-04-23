e10a4e3a00617699f19524f54a238643e4ddd4bf
Julio Muller <juliodolago@gmail.com>
2020-04-22 22:23:14 -0300

    Defined base layout and design

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hello, Vue!</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
 
-  components: {
-    HelloWorld,
-  },
+  components: {},
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100&display=swap');
+
+* {
+  font-family: 'Roboto Mono', monospace;
+}
+
+html, body {
+  margin: 0;
+  height: 100%;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+  background-color: #0F2027;
+  background-image: -webkit-linear-gradient(to right, #2C5364, #203A43, #0F2027);
+  background-image: linear-gradient(to right, #2C5364, #203A43, #0F2027);
 }
 </style>
